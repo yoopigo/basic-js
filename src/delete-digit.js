71c29c31a3a8a2f1a3dc84509f6d67c56d7865c1
Andrey <collerggggggg@gmail.com>
2025-01-05 03:34:09 +0500

    4.5 tasks

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  arr = []
+
+  let n1 = String(n);
+
+  for (let i = 0; i < n1.length; i++) {
+    let num = +(n1.slice(0, i) + n1.slice(i + 1))
+    arr.push(num)
+
+  }
+
+  return Math.max(...arr);
 }
 
 module.exports = {
--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let result = {};
+
+
+  for (let i = 0; i < domains.length; i++) {
+    let dom = domains[i].split('.').reverse();
+
+    let suffix = '';
+
+    for (let j = 0; j < dom.length; j++) {
+      suffix = dom[j] + '.' + suffix;
+      suffixReverse = suffix.split('.').reverse().join('.');
+      result[suffixReverse] = result[suffixReverse] ? result[suffixReverse] + 1 : 1;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
